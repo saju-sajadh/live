d023e669bf5d213918b0663cd1e8218b5167229e
saju-sajadh <sajadhtechosa@gmail.com>
2025-01-11 17:11:41 +0530

    ok

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:live_object_detection_ssd_mobilenet/ui/home_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Live Object Detection TFLite',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const HomeView(),
+      );
+}
